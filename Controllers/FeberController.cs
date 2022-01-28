060364fb6d7a9601e1af176ba2a9d781d54eee34
Tomas Danielsson <td_mbk@hotmail.com>
2022-01-28 14:05:37 +0100

    Add project files.

---
+++ Controllers/FeberController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Lexicon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Lexicon.Controllers
+{
+    public class FeberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CheckFever()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CheckFever(int temp)
+        {
+            ViewBag.Message = FeverModel.Check(temp);  // string !!!!
+            /*if(temp > 37)
+            ViewBag.Message = "Du har feber som är "+ temp+" grader";
+           
+            else ViewBag.Message = "Du är kärn frisk din rackare";*/
+
+            return View();
+        }
+    }
+}
---
+++ Models/FeverModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Lexicon.Models
+{
+    public class FeverModel
+    {
+        public static string Check(int temp)
+        {
+            string message;
+
+            if (temp > 37)
+            {
+                message = "Din temp är " + temp + " grader, och visar att du har feber";
+            }
+            else
+                message = "Du har ingen feber";
+            return message;
+        }
+    }
+}
